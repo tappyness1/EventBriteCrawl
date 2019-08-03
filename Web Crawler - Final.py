5c24fac4e4da86ea6478d3b7529d00894ea06aa3
Neo Ce Zheng <neocezheng@outlook.com>
2019-08-04 00:59:10 +0800

    Add files via upload

--- Web Crawler - Final.py
+++ Web Crawler - Final.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import urllib.request
 import csv
 from selenium import webdriver
@@ -74,9 +75,10 @@ for num in range(30):
                         else:
                             EventDetails.append(i.text)
         
-                    dict1[dictnum] = {'Event Name': event_name, 
-                         'Event Date': EventDetails[0],
-                         'Event Location': EventDetails[1],
+                    dict1[dictnum] = {'EventName': event_name, 
+                         'TicketPrice' : ticket_price,
+                         'EventDate': EventDetails[0],
+                         'EventLocation': EventDetails[1],
                          'Link': link
                          }
                     dictnum += 1
@@ -89,21 +91,45 @@ for num in range(30):
     except:
         pass
     time.sleep(2)
-    try:
-        driver.find_element_by_class_name('eds-l-pad-left-4').click()
-    except:
+    if driver.find_element_by_class_name('eds-l-pad-left-4').find_element_by_class_name('eds-btn').get_attribute('aria-disabled') == 'true':
         print ("No more pages")
         break
+    else: driver.find_element_by_class_name('eds-l-pad-left-4').click()
     time.sleep(2)
     
 data = pd.DataFrame.from_dict(dict1, orient = 'index')
 
-data['Event Date'] = data['Event Date'].str.replace('\n', ' ')
-data['Event Date'] = data['Event Date'].str.replace('Add to Calendar', '')
-data['Event Date'] = data['Event Date'].str.strip()
+data['EventDate'] = data['EventDate'].str.replace('\n', ' ')
+data['EventDate'] = data['EventDate'].str.replace('Add to Calendar', '')
+data['EventDate'] = data['EventDate'].str.strip()
 
-data['Event Location'] = data['Event Location'].str.replace('\n', ' ')
-data['Event Location'] = data['Event Location'].str.replace('View Map', '')
-data['Event Location'] = data['Event Location'].str.strip()
+data['EventLocation'] = data['EventLocation'].str.replace('\n', ' ')
+data['EventLocation'] = data['EventLocation'].str.replace('View Map', '')
+data['EventLocation'] = data['EventLocation'].str.strip()
 
-data.to_csv('mockdata.csv')
+# change all Free to blank
+data['TicketPrice'] = data.apply(lambda row: np.nan if row['TicketPrice'] == 'Free' else row['TicketPrice'], axis = 1)
+data['TicketPrice'] = data.apply(lambda row: np.nan if row['TicketPrice'] == '' else row['TicketPrice'], axis = 1)
+data = data.dropna(subset = ['TicketPrice'])
+data['TicketPrice'] = data['TicketPrice'].astype('str')
+# split the price based on TicketPrice
+#data[['Starting Price','Highest Price']] = data['TicketPrice'].str.split(' - ', expand = True)
+
+data[['StartingPrice', 'Nothing','HighestPrice']] = data['TicketPrice'].str.split(expand = True)
+data = data.drop(columns="Nothing")
+data['HighestPrice'].fillna(value = "", inplace = True)
+data['HighestPrice'].fillna(value = "", inplace = True)
+data['StartingPrice'] = data['StartingPrice'].apply(lambda x: ''.join([i for i in x if i in '1234567890.']))
+
+data['HighestPrice'] = data['HighestPrice'].apply(lambda x: ''.join([i for i in x if i in '1234567890.']))
+data['StartingPrice'] = data['StartingPrice'].apply(lambda x: 0 if x == '' else x)
+data['HighestPrice'] = data['HighestPrice'].apply(lambda x: 0 if x == '' else x)
+
+data['Keep'] = data.apply(lambda row: 'Yes' if float(row['StartingPrice']) > 300 or float(row['HighestPrice']) > 300 else 'No', axis = 1)
+# filter out if both starting price and highest price are below the minimum
+data = data[data['Keep'] == 'Yes']
+data = data.drop(columns = 'Keep')
+
+data.to_csv('results.csv', index = False)
+
+
